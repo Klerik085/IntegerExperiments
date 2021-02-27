3d576fa7fc01fe258d0140017a94bb917259e7c3
klerik <y.kislitsin85@gmail.com>
2021-02-27 16:34:05 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Container container = new Container();
+        container.count += 7843;
+        int sum = 0;
+
+    }
+
+    public Integer sumDigits(Integer number)
+    {
+        Integer.toString(number);
+        this.sum = number;
+
+
+
+        return 0;
+    }
+}
+
+    //Реализовать метод sumDigits, чтобы он считал сумму цыфр в числе number, используя 3 метода:
+    // Integer.toString(), .charAt() и Length(). Вывести в консоль результат.
